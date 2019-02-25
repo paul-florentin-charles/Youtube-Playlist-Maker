c587939c7598a389ebeb6f40e2bec7168d1e83e1
polochinoc <paul.charles@tutanota.com>
2019-02-25 01:16:37 +0100

    add utils

--- make_playlist.lua
+++ make_playlist.lua
@@ -1,24 +1,12 @@
 #!/usr/bin/env lua
 
-
 --[[ Youtube Playlist Creator ]]
 
 -- @author Paul Charles
 -- @release Module to create your own YouTube playlists without having an account
 
---- Shuffles a table
--- @return Same table with randomised ordering of elements
--- @param array Table representing an array
-function shuffle(array)
-   local r_idx = 1
-   math.randomseed(os.time())
-   for i = 1, #array - 1
-   do
-      r_idx = math.random(i + 1, #array)
-      array[i], array[r_idx] = array[r_idx], array[i]
-   end
-   return array
-end
+
+require "utils"
 
 --- Creates an URL correspond to a YouTube playlist
 -- Uses the ids contained in <ids>
